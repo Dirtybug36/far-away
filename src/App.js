698c7d2dfe2d4d091cf9c0c8260e1d1a7340d77c
Dirtybug36 <104801789+Dirtybug36@users.noreply.github.com>
2023-07-27 11:59:15 +0100

    header, input and list is made

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./index.css";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 function App() {
+  //lifting in parent so that it can be accesible for the children
+  const [items, setItems] = useState(initialItems);
+
+  //create a function so that the children can use setitems
+  function handleItems(item) {
+    setItems((prevItem) => [...prevItem, item]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1> 🏝️Far Away🛍️</h1>
+      <Form onAddItems={handleItems} />
+      <List items={items} />
     </div>
+  );
+}
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleForm(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItems = [{ description, quantity, packed: false, id: Date.now() }];
+    onAddItems(newItems);
+    setDescription("");
+    setQuantity(1);
+  }
+  //here in handle form if there is no any description the form is not submitted,new description will come from input and quantity will come from select and then it is added to onAdditems which has  setItems((item) => [...items, item]);
+
+  //create a array for the 'select' to use
+  const numbers = Array.from({ length: 20 }, (_, i) => i + 1);
+  return (
+    <form className="add-form" onSubmit={handleForm}>
+      <h3>What do you need for your 😊 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {numbers.map((number) => (
+          <option key={number} value={number}>
+            {number}
+          </option>
+        ))}
+      </select>
+      <input
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        type="text"
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+function List({ items }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((items) => (
+          <Listing items={items} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Listing({ items }) {
+  return (
+    <>
+      <li key={items.id}>
+        {items.quantity}- {items.description}
+      </li>
+    </>
   );
 }
 
